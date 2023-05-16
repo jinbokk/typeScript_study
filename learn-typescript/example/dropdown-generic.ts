b5f63c7b5d351065462d86e3c6ae3d19d17cf166
JINBOK LEE <eyelash1024@naver.com>
2023-05-16 21:22:02 +0900

    studying

--- learn-typescript/example/dropdown-generic.ts
+++ learn-typescript/example/dropdown-generic.ts
@@ -1,26 +1,37 @@
-interface Email {
-  value: string;
+interface DropdownItems<T> {
+  value: T;
   selected: boolean;
 }
 
-const emails: Email[] = [
+const emails: DropdownItems<string>[] = [
   { value: "naver.com", selected: true },
   { value: "gmail.com", selected: false },
   { value: "hanmail.net", selected: false },
 ];
 
-interface ProductNumber {
-  value: number;
-  selected: boolean;
-}
-
-const numberOfProducts: ProductNumber[] = [
+const numberOfProducts: DropdownItems<number>[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item: Email | ProductNumber) {
+// function createDropdownItem(item: DropdownItems<string> | DropdownItems<number>) {
+//   const option = document.createElement("option");
+//   option.value = item.value.toString();
+//   option.innerText = item.value.toString();
+//   option.selected = item.selected;
+//   return option;
+// }
+
+// function createDropdownItem(item: DropdownItems<string | number>) {
+//   const option = document.createElement("option");
+//   option.value = item.value.toString();
+//   option.innerText = item.value.toString();
+//   option.selected = item.selected;
+//   return option;
+// }
+
+function createDropdownItem<T>(item: DropdownItems<T>) {
   const option = document.createElement("option");
   option.value = item.value.toString();
   option.innerText = item.value.toString();
@@ -30,7 +41,7 @@ function createDropdownItem(item: Email | ProductNumber) {
 
 // NOTE: 이메일 드롭 다운 아이템 추가
 emails.forEach(function (email) {
-  const item = createDropdownItem(email);
+  const item = createDropdownItem<string>(email);
   const selectTag = document.querySelector("#email-dropdown");
   selectTag.appendChild(item);
 });
